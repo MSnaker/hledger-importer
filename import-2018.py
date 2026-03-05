f2662d855fdba9e9d4bd13691d773c64e6ca4271
Marco Bissaro <marco.bissaro@dorbit.space>
2026-03-05 23:05:13 +0100

    import 2018 half complete, missing recurring expenses

--- import-2018.py
+++ import-2018.py
@@ -1,5 +1,35 @@
-from openpyxl import Workbook
 from openpyxl import load_workbook
-wb = load_workbook(filename = "INPUT/Balance_2018.xlsx") 
+from datetime import datetime
 
-print(wb.sheetnames)
+wb = load_workbook(filename="INPUT/Balance_2018.xlsx")
+
+def len_str(*args) -> int:
+    totlen = 0
+    for arg in args:
+        if type(arg) != str:
+            raise TypeError
+        foo = len(arg)
+        totlen += foo
+
+    return totlen     
+
+sheetimport = wb['IMPORT']
+reason = "EXCEL IMPORT"
+source = "Marco:assets:BancoBPM"
+
+
+with open("./accounting/.hledger.journal", "a", encoding="utf-8") as fw:
+    for row in sheetimport.rows:
+        date = datetime.date(row[0].value)
+        destination = row[1].value
+        amount = row[2].value
+        line1 = f"\n{date} {reason}\n"
+
+        len2 = len_str(destination, str(amount)) + 8
+        len3 = len_str(source, str(amount)) + 9
+        padding = max(len2,len3)
+        
+        line2 = f"\t{destination:<{padding}}{amount}\n"
+        line3 = f"\t{source:<{padding-1}}-{amount}\n"
+        string_to_append = line1 + line2 + line3
+        fw.write(string_to_append)
