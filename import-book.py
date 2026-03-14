2e26db4555dea141e8d2f7a4ec4210c95de3288c
Marco Bissaro <marco.bissaro@dorbit.space>
2026-03-14 09:50:22 +0100

    painfully added ticket restaurant to transactions

--- import-book.py
+++ import-book.py
@@ -35,9 +35,11 @@ def get_transaction(row) -> tuple:
     destination = row[transaction_dest].value if int == type(transaction_dest) else transaction_dest
     source = row[transaction_source].value if int == type(transaction_source) else transaction_source
     description = row[transaction_description].value if int == type(transaction_description) else transaction_description
-    value = str(row[transaction_value].value)
+    value = row[transaction_value].value
+    ticket = row[transaction_ticket].value if int == type(transaction_description) else None
 
-    return (date, destination, source, description, value)
+
+    return (date, destination, source, description, value, ticket)
 
 
 ####################################################
@@ -56,7 +58,8 @@ transaction_date = conditional_get_col(conf['transaction date'])
 transaction_description = conditional_get_col(conf['transaction description'])
 transaction_source = conditional_get_col(conf['transaction source'])
 transaction_dest = conditional_get_col(conf['transaction dest'])
-transaction_value = conditional_get_col(conf['transaction_value'])
+transaction_value = conditional_get_col(conf['transaction value'])
+transaction_ticket = conditional_get_col(conf['transaction ticket'])
 try:
     data_range = range(conf['data']['start row'] - 1, conf['data']['end row'])
 except KeyError:
@@ -67,16 +70,30 @@ with open(journalfile, "a", encoding="utf-8") as fw:
 
     for i, row in enumerate(worksheet.rows):
         if i in data_range:
-            date, destination, source, description, value = get_transaction(row)
+            date, destination, source, description, value, ticket = get_transaction(row)
 
+            if (0 == ticket) or (ticket is None):
+                ticket_desc = ""
+                ticket_value = 0
+                value_source = value
+            else:
+                ticket_desc = 'Marco:assets:Ticket Restaurant'
+                ticket_value = 8 * ticket 
+                value_source = float(value) - ticket_value
+            
+            value_source_stringified = f"{value_source:.2f}"
+            value_dest_stringified = f"{value:.2f}"
             line1 = f"\n{date} {description}\n"
 
-            len2 = len_str(destination, str(value)) + 8
-            len3 = len_str(source, str(value)) + 9
-            padding = max(len2,len3)
+            len2 = len_str(destination, value_dest_stringified) + 8
+            len3 = len_str(source, value_source_stringified) + 9
+            len4 = len_str(ticket_desc, str(ticket_value)) + 9 if ticket is not None else 0
+            padding = max(len2, len3, len4)
             
-            line2 = f"\t{destination:<{padding}}{value}\n"
-            line3 = f"\t{source:<{padding-1}}-{value}\n"
-            string_to_append = line1 + line2 + line3
-            # print(string_to_append)
-            fw.write(string_to_append)
+            line2 = f"\t{destination:<{padding}}{value_dest_stringified}\n"
+            line3 = f"\t{source:<{padding-1}}-{value_source_stringified}\n"
+
+            line4 = f"\t{ticket_desc:<{padding-1}}-{ticket_value}" if (ticket is not None and 0 != ticket) else ""
+            string_to_append = line1 + line2 + line3 + line4
+            print(string_to_append)
+            # fw.write(string_to_append)
